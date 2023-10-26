47af6165c0ba6b92e50ea803a441d2b7f91478bc
cakocaito <cmsssilva@id.uff.br>
2023-10-26 02:08:01 -0300

    Mapa adicionado com localização na UFF Gragoata, utilizando react-native-maps

--- App.tsx
+++ App.tsx
@@ -1,20 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Mapa } from './src/screens/Mapa';
+import { useEffect, useState } from 'react';
+import MapView from 'react-native-maps';
+import { requestForegroundPermissionsAsync, getCurrentPositionAsync, LocationObject } from 'expo-location';
 
 export default function App() {
+  const[location, setLocation] = useState<LocationObject | null>(null);
+    
+  async function requestLocationPermissions(){
+    const { granted } = await requestForegroundPermissionsAsync();
+    
+    if(granted){
+        const currentPosition = await getCurrentPositionAsync();
+        setLocation(currentPosition);
+    }
+  }
+
+  useEffect(() => {
+    requestLocationPermissions(); 
+  }, [])
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style = {{
+        flex: 1, width:"100%"
+      }}>
+        {
+        location && 
+          <MapView style = {{
+          flex:1, width:"100%"
+          }}
+          initialRegion={{
+            latitude: -22.89845121622674,
+            longitude: -43.131635942333034,
+            latitudeDelta: 0.008,
+            longitudeDelta: 0.008
+          }}
+          />
+        }
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
---
+++ src/screens/Mapa/styles.ts
@@ -0,0 +1,6 @@
+import styled from "styled-components/native";
+
+export const MapaEstilo = styled.View`
+    flex: 1;
+    width: 100;
+`;
